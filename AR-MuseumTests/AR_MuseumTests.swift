b355014059c7c119749bf53e7bb83775cfc34c13
jaeden-xu <jzxu98@outlook.com>
2025-07-15 19:44:07 +0800

    Initial Commit

---
+++ AR-MuseumTests/AR_MuseumTests.swift
@@ -0,0 +1,17 @@
+//
+//  AR_MuseumTests.swift
+//  AR-MuseumTests
+//
+//  Created by JaedenXu on 2025/7/15.
+//
+
+import Testing
+@testable import AR_Museum
+
+struct AR_MuseumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
